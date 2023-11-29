9baf33cfbc0d7b216bbd36031498752344517593
Naga_venkata_Sai_Kommu <knvsai0007@gmail.com>
2023-11-29 11:18:59 +0530

    python files used in devops life cycle

---
+++ lists.py
@@ -0,0 +1,10 @@
+list = [[1, 1, 1, 0, 0],
+      [0, 1, 0, 0, 1],
+      [1, 0, 1, 1, 0],
+      [0, 1, 0, 0, 1],
+      [1, 1, 1, 0, 0]]
+unique_list = []
+for i in list:
+    if i in unique_list:
+        unique_list.append()
+print(unique_list)
